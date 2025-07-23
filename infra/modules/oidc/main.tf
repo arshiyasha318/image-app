33f5ac7bff804fcdfd07c14d34a2e36825204ac3
Arshu <arshiyasha318@gmail.com>
2025-07-23 15:14:08 +0530

    updated oidc module  main.tf

--- infra/modules/oidc/main.tf
+++ infra/modules/oidc/main.tf
@@ -8,10 +8,17 @@ data "aws_eks_cluster_auth" "this" {
   name = var.cluster_name
 }
 
+# Data source to fetch the TLS certificate and compute the SHA-1 thumbprint for the OIDC issuer
+# This is required for the thumbprint_list in the OIDC provider resource
+# Requires the tls provider (add to required_providers if not present)
+data "tls_certificate" "oidc_thumbprint" {
+  url = data.aws_eks_cluster.this.identity[0].oidc[0].issuer
+}
+
 # OIDC provider resource for enabling IRSA (IAM Roles for Service Accounts)
 resource "aws_iam_openid_connect_provider" "this" {
   client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = [data.aws_eks_cluster.this.certificate_authority[0].data]
+  thumbprint_list = [data.tls_certificate.oidc_thumbprint.certificates[0].sha1_fingerprint]
   url             = data.aws_eks_cluster.this.identity[0].oidc[0].issuer
 
   tags = {
