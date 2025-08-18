9173be8def0b0ed7c092b7777b5d2ec53ad245b9
Arshu <arshiyasha318@gmail.com>
2025-08-18 21:48:35 +0530

    Remove commented-out required providers block in Terraform configuration

--- infra/main.tf
+++ infra/main.tf
@@ -155,9 +155,6 @@ resource "null_resource" "apply_k8s_manifests" {
 # Terraform block specifying required providers
 terraform { 
   # required_providers {
-  #   aws = {
-  #     source = "hashicorp/aws"
-  #     version = ">= 5.95.0, < 6.0.0"
-  #   }
+  
   # }
-}
+} 
